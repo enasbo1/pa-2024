a04ec9f9e249772044433c175cc709a27fd6450e
lyess <lyess_boufaden@yahoo.com>
2024-05-13 01:10:25 +0200

    [FEAT][FRONT] add function and css on chat

--- application/front/src/app/shared/foundation/chat/chat.component.ts
+++ application/front/src/app/shared/foundation/chat/chat.component.ts
@@ -1,6 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ChatObject} from "./chatObject";
-import {TranslatorService} from "../../base-shared/translator.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { ChatObject } from "./chatObject";
+import { TranslatorService } from "../../base-shared/translator.service";
 
 @Component({
   selector: 'pm-chat',
@@ -8,23 +8,22 @@ import {TranslatorService} from "../../base-shared/translator.service";
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-  @Input() items:ChatObject[] = [];
-  sortedMessages:ChatObject[] = [];
-  constructor(public translator:TranslatorService) { }
+  @Input() items: ChatObject[] = [];
+  sortedMessages: ChatObject[] = [];
+
+  constructor(public translator: TranslatorService) {}
 
   ngOnInit(): void {
-
-
     this.sortMessages(); //fonction de trie utilisée
   }
 
   sortMessages(): void {
-    // Trie les messages par date du plus ancien au plus récent
     this.sortedMessages = this.items.sort((a, b) => {
       return new Date(a.date).getTime() - new Date(b.date).getTime();
     });
   }
+
+  getInitials(userName: string): string {
+    return userName.split(' ').map(n => n[0]).join('');
+  }
 }
-
-
-
