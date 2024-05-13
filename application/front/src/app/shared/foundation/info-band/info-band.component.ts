46f6f68b78d7d7d42948870f63c042eba27ce662
lyess <lyess_boufaden@yahoo.com>
2024-05-13 22:49:41 +0200

    [FEAT][FRONT[ERRORS] ajout d'une bulle utilsateur chat mal calibrée et ajout d'un bandeau non fonctionnel

--- application/front/src/app/shared/foundation/info-band/info-band.component.ts
+++ application/front/src/app/shared/foundation/info-band/info-band.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BandObject } from './bandObject';
 
 @Component({
-  selector: 'pm-info-band',
+  selector: 'app-info-band',
   templateUrl: './info-band.component.html',
-  styleUrls: ['./info-band.component.scss']
+  styleUrls: ['./info-band.component.css']
 })
-export class InfoBandComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class InfoBandComponent {
+  @Input() bannerData: BandObject = {
+    title: '',          // Valeur par défaut vide pour éviter undefined
+    description: '',    // Valeur par défaut vide pour éviter undefined
+    link: '',           // Valeur par défaut vide pour éviter undefined
+    priority: 0         // Assurez-vous que priority a une valeur par défaut qui ne déclenche pas high-priority
+  };
 }
+
