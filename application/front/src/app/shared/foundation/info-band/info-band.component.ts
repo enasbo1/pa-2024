05548a3a3d1ae79fc49e5e3c71ec7413dd1d7e6b
enasbo1 <irenee.du.gardin@gmail.com>
2024-05-14 21:06:41 +0200

    [FEAT][FRONT][HELP] correction

--- application/front/src/app/shared/foundation/info-band/info-band.component.ts
+++ application/front/src/app/shared/foundation/info-band/info-band.component.ts
@@ -4,14 +4,9 @@ import { BandObject } from './bandObject';
 @Component({
   selector: 'app-info-band',
   templateUrl: './info-band.component.html',
-  styleUrls: ['./info-band.component.css']
+  styleUrls: ['./info-band.component.scss']
 })
 export class InfoBandComponent {
-  @Input() bannerData: BandObject = {
-    title: '',          // Valeur par défaut vide pour éviter undefined
-    description: '',    // Valeur par défaut vide pour éviter undefined
-    link: '',           // Valeur par défaut vide pour éviter undefined
-    priority: 0         // Assurez-vous que priority a une valeur par défaut qui ne déclenche pas high-priority
-  };
+  @Input() bannerData?: BandObject
 }
 
