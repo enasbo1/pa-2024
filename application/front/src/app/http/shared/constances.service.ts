e3b227eb532511a4c4b85e1fae83c8263466ed06
lyess <lyess_boufaden@yahoo.com>
2024-07-10 23:54:41 +0200

    [FEAT][FRONT] changement page d'accueil

--- application/front/src/app/http/shared/constances.service.ts
+++ application/front/src/app/http/shared/constances.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstancesService {
-  static api_url:string = "http://localhost:8081/index.php"
+  static api_url:string = "http://162.19.243.218:8081/index.php"
 
   constructor() { }
 }
--- application/front/src/app/view/exemple/home/welcome.component.ts
+++ application/front/src/app/view/exemple/home/welcome.component.ts
@@ -1,18 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {GlobalService} from "../../../shared/global.service";
-import {TranslatorService} from "../../../shared/base-shared/translator.service";
+import { Component, OnInit } from '@angular/core';
+import { GlobalService } from "../../../shared/global.service";
+import { TranslatorService } from "../../../shared/base-shared/translator.service";
 
 @Component({
   templateUrl: './welcome.component.html'
 })
-export class WelcomeComponent implements OnInit{
-  public pageTitle = 'Welcome';
-  protected readonly GlobalService = GlobalService;
+export class WelcomeComponent implements OnInit {
+  public pageTitle: string = '';
 
-  constructor(protected translator:TranslatorService) {
-  }
+  constructor(protected translator: TranslatorService) { }
 
-  ngOnInit(){
-    GlobalService.pageName = "Home"
+  ngOnInit() {
+    GlobalService.pageName = "Home";  // Définir le nom de la page
+    this.pageTitle = GlobalService.pageName;  // Utiliser le nom de la page comme titre
   }
 }
